f6d3d59b024f0ea06c909fa6a12ed0a90eae0d3c
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-02-17 18:12:01 +0100

    Directory cleanup

--- capreolus/searcher/__init__.py
+++ capreolus/searcher/__init__.py
@@ -1,210 +1,98 @@
-import json
 import os
-
-from collections import defaultdict
+import subprocess
 
 import numpy as np
-import pytrec_eval
 
-from capreolus.utils.common import register_component_module, import_component_modules
+from capreolus.registry import ModuleBase, RegisterableModule, Dependency, MAX_THREADS
+from capreolus.utils.common import Anserini
 from capreolus.utils.loginit import get_logger
 
 logger = get_logger(__name__)  # pylint: disable=invalid-name
 
 
-class Searcher:
-    """ Module responsible for searching an Index. Searchers are usually coupled to an Index module (e.g., AnseriniIndex). """
+class Searcher(ModuleBase, metaclass=RegisterableModule):
+    """the module base class"""
 
-    ALL = {}
+    module_type = "searcher"
 
-    def __init__(self, index, collection, run_path, pipe_config):
-        self.index = index
-        self.collection = collection
-        self.run_path = run_path
-        self.pipeline_config = pipe_config
+
+class AnseriniSearcherMixIn:
+    """ MixIn for searchers that use Anserini's SearchCollection script """
+
+    def _anserini_query_from_file(self, topicsfn, anserini_param_str, output_base_path):
+        if not os.path.exists(topicsfn):
+            raise IOError(f"could not find topics file: {topicsfn}")
+
+        donefn = os.path.join(output_base_path, "done")
+        if os.path.exists(donefn):
+            logger.debug(f"skipping Anserini SearchCollection call because path already exists: {donefn}")
+            return
+
+        self["index"].create_index()
+
+        os.makedirs(output_base_path, exist_ok=True)
+        output_path = os.path.join(output_base_path, "searcher")
+
+        # add stemmer and stop options to match underlying index
+        indexopts = f"-stemmer {self['index'].cfg['stemmer']}"
+        if self["index"].cfg["indexstops"]:
+            indexopts += " -keepstopwords"
+
+        index_path = self["index"].get_index_path()
+        anserini_fat_jar = Anserini.get_fat_jar()
+        cmd = f"java -classpath {anserini_fat_jar} -Xms512M -Xmx31G -Dapp.name=SearchCollection io.anserini.search.SearchCollection -topicreader Trec -index {index_path} {indexopts} -topics {topicsfn} -output {output_path} -inmem -threads {MAX_THREADS} {anserini_param_str}"
+        logger.info("Anserini writing runs to %s", output_path)
+        logger.debug(cmd)
+
+        app = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
+
+        # Anserini output is verbose, so ignore DEBUG log lines and send other output through our logger
+        for line in app.stdout:
+            Anserini.filter_and_log_anserini_output(line, logger)
+
+        app.wait()
+        if app.returncode != 0:
+            raise RuntimeError("command failed")
+
+        with open(donefn, "wt") as donef:
+            print("done", file=donef)
+
+
+class BM25(Searcher, AnseriniSearcherMixIn):
+    """ BM25 with fixed k1 and b. """
+
+    name = "BM25"
+    dependencies = {"index": Dependency(module="index", name="anserini")}
 
     @staticmethod
     def config():
-        return locals().copy()  # ignored by sacred
+        b = 0.4  # controls document length normalization
+        k1 = 0.9  # controls term saturation
 
-    @classmethod
-    def register(cls, subcls):
-        return register_component_module(cls, subcls)
+    def query_from_file(self, topicsfn, output_path):
+        bs = [self.cfg["b"]]
+        k1s = [self.cfg["k1"]]
+        bstr = " ".join(str(x) for x in bs)
+        k1str = " ".join(str(x) for x in k1s)
+        anserini_param_str = f"-bm25 -b {bstr} -k1 {k1str}"
+        self._anserini_query_from_file(topicsfn, anserini_param_str, output_path)
 
-    @staticmethod
-    def load_trec_run(fn):
-        run = defaultdict(dict)
-        with open(fn, "rt") as f:
-            for line in f:
-                line = line.strip()
-                if len(line) > 0:
-                    qid, _, docid, rank, score, desc = line.split(" ")
-                    run[qid][docid] = float(score)
-        return run
 
-    @staticmethod
-    def write_trec_run(preds, outfn):
-        count = 0
-        with open(outfn, "wt") as outf:
-            for qid in sorted(preds):
-                rank = 1
-                for docid, score in sorted(preds[qid].items(), key=lambda x: x[1], reverse=True):
-                    print(f"{qid} Q0 {docid} {rank} {score} capreolus", file=outf)
-                    rank += 1
-                    count += 1
+class BM25Grid(Searcher, AnseriniSearcherMixIn):
+    """ BM25 with a grid search for k1 and b. Search is from 0.1 to bmax/k1max in 0.1 increments """
 
-    def exists(self):
-        return os.path.exists(os.path.join(self.run_path, "done"))
-
-    def _query_index():
-        raise NotImplementedError()
-
-    def create(self):
-        self.index.create(self.pipeline_config)
-        if self.exists():
-            return
-
-        # TODO check for and remove incomplete searcher files? then we can use -skipexists
-        self._query_index()
-        with open(os.path.join(self.run_path, "done"), "wt") as donef:
-            print("done", file=donef)
-
-    def search_run_iter(self, load_run=True):
-        self.create()
-        for fn in os.listdir(self.run_path):
-            if fn == "done" or fn.endswith(".metrics"):
-                continue
-            fn = os.path.join(self.run_path, fn)
-            if load_run:
-                yield self.load_trec_run(fn)
-            else:
-                yield fn
-
-    def search_run_metrics(self, fn, evaluator, qids):
-        cachefn = fn + ".metrics"
-        try:
-            with open(cachefn, "rt") as cachef:
-                metrics_cache = json.load(cachef)
-        except:
-            metrics_cache = {}
-
-        # return from cache if available
-        if all(qid in metrics_cache for qid in qids):
-            # pytrec_eval does not include qids with no qrels in its output dict, so we also skip them here
-            return {qid: metrics_cache[qid] for qid in qids if metrics_cache[qid] != "NONE"}
-
-        # else calculate and cache the metrics
-        run = self.load_trec_run(fn)
-        metrics = evaluator.evaluate(run)
-
-        # update works for adding missing qids, but will not work for missing metrics
-        # (i.e., the old metrics will be overwritten due to dict nesting)
-        metrics_cache.update(metrics)
-        # add "NONE" placeholders for qids that pytrec_eval omitted (due to having no relevant docs in qrels)
-        metrics_cache.update({qid: "NONE" for qid in qids if qid not in metrics_cache})
-        with open(cachefn, "wt") as outf:
-            json.dump(metrics_cache, outf, indent=4)
-
-        # pytrec_eval does not include qids with no qrels in its output dict, so we also skip them here
-        return {qid: metrics_cache[qid] for qid in qids if metrics_cache[qid] != "NONE"}
-
-    def crossvalidated_ranking(self, dev_qids, test_qids, metric="map", full_run=False):
-        """ Return a ranking for queries in test_qids using parameters chosen using the queries in dev_qids """
-
-        valid_metrics = {"P", "map", "map_cut", "ndcg_cut", "Rprec", "recip_rank"}
-        cut_points = [5, 10, 15, 20, 30, 100, 200, 500, 1000]
-        # the metrics we expect pytrec_eval to output (after expanding _cut)
-        expected_metrics = {m for m in valid_metrics if not m.endswith("_cut") and m != "P"} | {
-            m + "_" + str(cutoff) for cutoff in cut_points for m in valid_metrics if m.endswith("_cut") or m == "P"
-        }
-
-        if metric in ["ndcg", "ndcg_cut"]:
-            mkey = "ndcg_cut_20"
-        elif metric in expected_metrics:
-            mkey = metric
-        else:
-            raise RuntimeError("requested metric %s is not one of the supported metrics: %s" % (metric, sorted(expected_metrics)))
-        avg_metric = lambda run_metrics: np.mean([qid[mkey] for qid in run_metrics.values()])
-
-        dev_qrels = {qid: labels for qid, labels in self.collection.qrels.items() if qid in dev_qids}
-        dev_eval = pytrec_eval.RelevanceEvaluator(dev_qrels, valid_metrics)
-        best_metric, best_run_fn = -np.inf, None
-        for search_run_fn in self.search_run_iter(load_run=False):
-            run_metrics = self.search_run_metrics(search_run_fn, dev_eval, dev_qids)
-            mavgp = avg_metric(run_metrics)
-            # assert that all qids are in what we get back from the evaluator?
-            # -> looks like it returns a metric of 0 for qids with all 0 labels, so this should work fine
-            if mavgp > best_metric:
-                best_metric = mavgp
-                best_run_fn = search_run_fn
-
-        best_run = self.load_trec_run(best_run_fn)
-        if full_run:
-            test_ranking = best_run
-        else:
-            test_ranking = {qid: v for qid, v in best_run.items() if qid in test_qids}
-        return test_ranking
+    name = "BM25Grid"
+    dependencies = {"index": Dependency(module="index", name="anserini")}
 
     @staticmethod
-    def interpolate_runs(run1, run2, qids, alpha):
-        out = {}
-        for qid in qids:
-            out[qid] = {}
-            assert len(run1[qid]) == len(run2[qid])
+    def config():
+        bmax = 1.0  # maximum b value to include in grid search (starting at 0.1)
+        k1max = 1.0  # maximum k1 value to include in grid search (starting at 0.1)
 
-            min1, max1 = min(run1[qid].values()), max(run1[qid].values())
-            min2, max2 = min(run2[qid].values()), max(run2[qid].values())
-            mu1, std1 = np.mean([*run1[qid].values()]), np.std([*run1[qid].values()])
-            mu2, std2 = np.mean([*run2[qid].values()]), np.std([*run2[qid].values()])
-            for docid, score1 in run1[qid].items():
-                if docid not in run2[qid]:
-                    score2 = min2
-                    print(f"WARNING: missing {qid} {docid}")
-                else:
-                    score2 = run2[qid][docid]
-                score1 = (score1 - min1) / (max1 - min1)
-                score2 = (score2 - min2) / (max2 - min2)
-                # score1 = (score1 - mu1) / std1
-                # score2 = (score2 - mu2) / std2
-                out[qid][docid] = alpha * score1 + (1 - alpha) * score2
-
-        return out
-
-    @staticmethod
-    def crossvalidated_interpolation(dev, test, metric):
-        """ Return an interpolated ranking """
-
-        # TODO refactor out (shared with crossvalidated_ranking)
-        valid_metrics = {"P", "map", "map_cut", "ndcg_cut", "Rprec", "recip_rank"}
-        cut_points = [5, 10, 15, 20, 30, 100, 200, 500, 1000]
-        # the metrics we expect pytrec_eval to output (after expanding _cut)
-        expected_metrics = {m for m in valid_metrics if not m.endswith("_cut") and m != "P"} | {
-            m + "_" + str(cutoff) for cutoff in cut_points for m in valid_metrics if m.endswith("_cut") or m == "P"
-        }
-
-        if metric in ["ndcg", "ndcg_cut"]:
-            mkey = "ndcg_cut_20"
-        elif metric in expected_metrics:
-            mkey = metric
-        else:
-            raise RuntimeError("requested metric %s is not one of the supported metrics: %s" % (metric, sorted(expected_metrics)))
-        avg_metric = lambda run_metrics: np.mean([qid[mkey] for qid in run_metrics.values()])
-
-        assert len(set(dev["qrels"].keys()).intersection(test["qrels"].keys())) == 0
-        dev_eval = pytrec_eval.RelevanceEvaluator(dev["qrels"], valid_metrics)
-        best_metric, best_alpha = -np.inf, None
-        for alpha in np.arange(0, 1.001, 0.05):
-            run_metrics = dev_eval.evaluate(
-                Searcher.interpolate_runs(dev["reranker"], dev["searcher"], dev["qrels"].keys(), alpha)
-            )
-            mavgp = avg_metric(run_metrics)
-            if mavgp > best_metric:
-                best_metric = mavgp
-                best_alpha = alpha
-
-        test_run = Searcher.interpolate_runs(test["reranker"], test["searcher"], test["qrels"].keys(), best_alpha)
-        dev_run = Searcher.interpolate_runs(dev["reranker"], dev["searcher"], dev["qrels"].keys(), best_alpha)
-        return (best_alpha, test_run, dev_run)
-
-
-import_component_modules("searcher")
+    def query_from_file(self, topicsfn, output_path):
+        bs = np.around(np.arange(0.1, self.cfg["bmax"] + 0.1, 0.1), 1)
+        k1s = np.around(np.arange(0.1, self.cfg["k1max"] + 0.1, 0.1), 1)
+        bstr = " ".join(str(x) for x in bs)
+        k1str = " ".join(str(x) for x in k1s)
+        anserini_param_str = f"-bm25 -b {bstr} -k1 {k1str}"
+        self._anserini_query_from_file(topicsfn, anserini_param_str, output_path)
--- capreolus/tests/test_pipeline.py
+++ capreolus/tests/test_pipeline.py
@@ -1,162 +1,35 @@
-import os
-
-import pytest
-from pymagnitude import Magnitude
-from pytest_mock import mocker
-import sacred
-
-from capreolus.benchmark.robust04 import Robust04Benchmark
-from capreolus.collection import Collection
-from capreolus.extractor.embedtext import EmbedText
-from capreolus.index.anserini import AnseriniIndex
-from capreolus.reranker.KNRM import KNRM
-from capreolus.reranker.PACRR import PACRR
 from capreolus.pipeline import Pipeline
-from capreolus.searcher.bm25 import BM25Grid
-from capreolus.utils.common import forced_types
 
 
-def test_get_parameters_to_module():
-    pipeline = Pipeline({})
-    ex = sacred.Experiment("capreolus")
+def test_extract_choices_from_argv():
+    # manually assign these to avoid calling import_module
+    pipeline = Pipeline([], {})
+    pipeline.module_order = ["m1", "m2"]
+    pipeline.module_defaults = {"m1": "cls1", "m2": "cls2"}
 
-    parameters_to_module = pipeline.get_parameters_to_module(ex)
-    assert parameters_to_module == {
-        "collection": "module",
-        "index": "module",
-        "searcher": "module",
-        "benchmark": "module",
-        "reranker": "module",
-        "expid": "stateless",
-        "earlystopping": "stateless",
-        "predontrain": "stateless",
-        "fold": "stateless",
-        "maxdoclen": "pipeline",
-        "maxqlen": "pipeline",
-        "batch": "pipeline",
-        "niters": "pipeline",
-        "itersize": "pipeline",
-        "gradacc": "pipeline",
-        "lr": "pipeline",
-        "seed": "pipeline",
-        "sample": "pipeline",
-        "softmaxloss": "pipeline",
-        "dataparallel": "pipeline",
-    }
+    arg_prefix = ["foo.py", "with"]
+
+    choices = pipeline._extract_choices_from_argv(arg_prefix)
+    assert choices == {}
+
+    choices = pipeline._extract_choices_from_argv(arg_prefix + "m1=foo1 m2=foo2 m3=foo3".split())
+    assert choices == {"m1": "foo1", "m2": "foo2"}
 
 
-def test_get_parameter_types(mocker):
-    pipeline = Pipeline({})
-    ex = sacred.Experiment("capreolus")
+def test_rewrite_argv_for_ingredients():
+    # manually assign these to avoid calling import_module
+    pipeline = Pipeline([], {})
+    pipeline.module_order = ["m1", "m2"]
+    pipeline.module_defaults = {"m1": "cls1", "m2": "cls2"}
 
-    def mock_config(method_that_generates_input_dict):
-        input_dict = method_that_generates_input_dict()
+    arg_prefix = ["foo.py", "with"]
 
-        # Just messing with the types to make sure that get_parameter_types does what it should
-        input_dict.update({"index": None, "niters": True})
-        return lambda: input_dict
+    rewritten_args = pipeline._rewrite_argv_for_ingredients(arg_prefix)
+    assert rewritten_args == arg_prefix
 
-    mocker.patch.object(ex, "config", mock_config)
-    parameter_types = pipeline.get_parameter_types(ex)
-    assert parameter_types == {
-        "pipeline": type("string"),  # "pipeline" key is added by the method
-        "collection": type("robust04"),
-        "earlystopping": forced_types[type(True)],
-        "index": forced_types[type(None)],
-        "searcher": type("bm25grid"),
-        "benchmark": type("robust04.title.wsdm20demo"),
-        "reranker": type("PACRR"),
-        "expid": type("debug"),
-        "predontrain": forced_types[type(True)],
-        "fold": type("s1"),
-        "maxdoclen": type(800),
-        "maxqlen": type(4),
-        "batch": type(32),
-        "niters": forced_types[type(True)],
-        "itersize": type(4096),
-        "gradacc": type(1),
-        "lr": type(0.001),
-        "seed": type(123_456),
-        "sample": type("simple"),
-        "softmaxloss": forced_types[type(True)],
-        "dataparallel": type("none"),
-    }
+    rewritten_args = pipeline._rewrite_argv_for_ingredients(arg_prefix + "m1=foo1 m2=foo2 m3=foo3".split())
+    assert rewritten_args == arg_prefix + "m1._name=foo1 m2._name=foo2 m3=foo3".split()
 
 
-def test_get_module_to_class():
-    pipeline = Pipeline({})
-    module_choices = {"reranker": "KNRM"}  # default is PACRR
-
-    module2class = pipeline.get_module_to_class({})
-    assert module2class["collection"].__class__ == Collection
-    assert module2class["index"].__class__ == AnseriniIndex.__class__
-    assert module2class["searcher"].__class__ == BM25Grid.__class__
-    assert module2class["benchmark"].__class__ == Robust04Benchmark.__class__
-    assert module2class["reranker"].__class__ == PACRR.__class__
-
-    module2class = pipeline.get_module_to_class(module_choices)
-    assert module2class["reranker"].__class__ == KNRM.__class__
-
-
-def test_get_parameters_to_module_including_missing_and_extractors():
-    """
-        Calls Pipeline.__init__() which in turn calls
-        1. self.get_parameters_to_module
-        2. get_parameters_to_module_for_missing_parameters
-        3. get_parameters_to_module_for_feature_parameters
-    """
-    pipeline = Pipeline({})
-    ex = sacred.Experiment("capreolus")
-
-    # parameters_to_module, parameter_types = pipeline.get_parameters_to_module_for_missing_parameters(ex)
-
-    assert pipeline.parameters_to_module == {
-        "collection": "module",
-        "benchmark": "module",
-        "reranker": "module",
-        "expid": "stateless",
-        "predontrain": "stateless",
-        "earlystopping": "stateless",
-        "maxdoclen": "pipeline",
-        "maxqlen": "pipeline",
-        "batch": "pipeline",
-        "niters": "pipeline",
-        "itersize": "pipeline",
-        "gradacc": "pipeline",
-        "lr": "pipeline",
-        "seed": "pipeline",
-        "sample": "pipeline",
-        "softmaxloss": "pipeline",
-        "dataparallel": "pipeline",
-        # AnseriniIndex specific config
-        "stemmer": "index",
-        "indexstops": "index",
-        # BM25Grid specific config
-        "index": "module",
-        # Robust04Benchmark specific config
-        "fold": "stateless",
-        "searcher": "module",
-        "rundocsonly": "benchmark",
-        # PACRR specific config
-        "mingram": "reranker",
-        "maxgram": "reranker",
-        "nfilters": "reranker",
-        "idf": "reranker",
-        "kmax": "reranker",
-        "combine": "reranker",
-        "nonlinearity": "reranker",
-        # EmbedText specific config
-        "embeddings": "extractor",
-        "keepstops": "extractor",
-    }
-
-
-def test_check_for_invalid_keys():
-    pipeline = Pipeline({})
-    ex = sacred.Experiment("capreolus")
-    pipeline.check_for_invalid_keys()
-
-    pipeline.parameters_to_module["foo_bar"] = "reranker"
-
-    with pytest.raises(ValueError):
-        pipeline.check_for_invalid_keys()
+def test_create_experiment():  # TODO
+    pass
