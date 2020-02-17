7d83182b771b510f4e23e4306a6a96df5ab2cf9d
Andrew Yates <andrew.yates@gmail.com>
2020-02-17 10:29:02 +0100

    move anserini output filter to utility class

--- capreolus/skeleton/utils/common.py
+++ capreolus/skeleton/utils/common.py
@@ -1,7 +1,6 @@
 import hashlib
-import importlib
+import logging
 import os
-import sysconfig
 import requests
 import sys
 from glob import glob
@@ -25,6 +24,29 @@ class Anserini:
                     return max(fat_jar_path, key=os.path.getctime)
 
         raise Exception("could not find anserini fat jar")
+
+    @classmethod
+    def filter_and_log_anserini_output(cls, line, logger):
+        """ Ignore DEBUG lines and require other lines pass our logging level """
+        fields = line.strip().split()
+
+        # is this a log line?
+        # at least 5 fields should exist
+        # (0) date field should be 10 digits and begin with 20. e.g. 2020-02-14
+        # (3) function field should begin with [
+        if len(fields) > 5 and len(fields[0]) == 10 and fields[3][0] == "[":
+            # skip debug messages
+            if fields[2] == "DEBUG":
+                msg = None
+            else:
+                loglevel = logging._nameToLevel.get(fields[2], 40)
+                msg = " ".join(fields[3:])
+        else:
+            loglevel = logging._nameToLevel["WARNING"]
+            msg = line.strip()
+
+        if msg:
+            logger.log(loglevel, "[AnseriniProcess] %s", msg)
 
 
 def download_file(url, outfn, expected_hash=None):
