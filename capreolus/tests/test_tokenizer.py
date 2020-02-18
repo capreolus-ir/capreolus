9c42ab876804ee95193654cc9134d1740e9c32d4
Crystina <xzhangbx@ust.hk>
2020-02-18 18:51:32 -0500

    first draft of tokenizer

---
+++ capreolus/tests/test_tokenizer.py
@@ -0,0 +1,15 @@
+from capreolus.collection import DummyCollection
+from capreolus.index import AnseriniIndex
+from capreolus.tokenizer import AnseriniTokenizer
+
+
+def test_anserini_tokenzier():
+    cfg = {"_name": "anserini", "keepstops": True, "stemmer": "none"}
+    tokenizer = AnseriniTokenizer(cfg)
+
+    index = AnseriniIndex({"_name": "anserini", "indexstops": False, "stemmer": "porter"})
+    index.modules["collection"] = DummyCollection({"_name": "dummy"})
+    index.create_index()
+
+    docs = index.get_docs(["LA010189-0001"])
+    print(tokenizer.tokenize(docs))
