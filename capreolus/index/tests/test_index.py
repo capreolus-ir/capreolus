51f47d3eec6ae639bab2d70211508bfb3423719e
Crystina <xzhangbx@ust.hk>
2020-02-19 17:34:49 -0500

    run black

--- capreolus/index/tests/test_index.py
+++ capreolus/index/tests/test_index.py
@@ -20,7 +20,7 @@ def test_anserini_get_docs(tmpdir_as_cache, dummy_index):
     docs = dummy_index.get_docs(["LA010189-0001", "LA010189-0002"])
     assert docs == [
         "Dummy Dummy Dummy Hello world, greetings from outer space!",
-        "Dummy Dummy Dummy Hello world, greetings from outer space!"
+        "Dummy Dummy Dummy Hello world, greetings from outer space!",
     ]
 
 
@@ -28,8 +28,7 @@ def test_anserini_get_df(tmpdir_as_cache, dummy_index):
     df = dummy_index.getdf("hello")
     assert df == 2
 
+
 def test_anserini_get_idf(tmpdir_as_cache, dummy_index):
     idf = dummy_index.getidf("hello")
     assert idf == 0.1823215567939546
-
-
