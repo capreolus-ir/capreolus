baf711141cb7ac979446b7ed4877a589c2dcc155
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-03-17 16:51:18 +0100

    TK - add masking

--- capreolus/reranker/tests/test_rerankers.py
+++ capreolus/reranker/tests/test_rerankers.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import pytest
+import torch
 from pymagnitude import Magnitude
 
 from capreolus.benchmark import DummyBenchmark
@@ -170,7 +171,8 @@ def test_tk(dummy_index, tmpdir, tmpdir_as_cache, monkeypatch):
             "ffdim": 100,
             "numlayers": 2,
             "numattheads": 4,
-            "alpha": 0.5
+            "alpha": 0.5,
+            "usemask": False
         }
     )
     trainer = PytorchTrainer(
@@ -230,3 +232,77 @@ def test_tk(dummy_index, tmpdir, tmpdir_as_cache, monkeypatch):
     )
 
     assert os.path.exists(Path(tmpdir) / "train" / "dev.best")
+
+
+def test_tk_get_mask(tmpdir, dummy_index, monkeypatch):
+    def fake_magnitude_embedding(*args, **kwargs):
+        return Magnitude(None)
+
+    monkeypatch.setattr(EmbedText, "_get_pretrained_emb", fake_magnitude_embedding)
+
+    reranker = TK(
+        {
+            "gradkernels": True,
+            "scoretanh": False,
+            "singlefc": True,
+            "projdim": 32,
+            "ffdim": 100,
+            "numlayers": 2,
+            "numattheads": 4,
+            "alpha": 0.5,
+            "usemask": True
+        }
+    )
+    reranker.modules["extractor"] = EmbedText(
+        {
+            "_name": "embedtext",
+            "embeddings": "glove6b",
+            "zerounk": False,
+            "calcidf": True,
+            "maxqlen": 4,
+            "maxdoclen": 800,
+            "usecache": False,
+        }
+    )
+    extractor = reranker.modules["extractor"]
+    extractor.modules["index"] = dummy_index
+    tok_cfg = {"_name": "anserini", "keepstops": True, "stemmer": "none"}
+    tokenizer = AnseriniTokenizer(tok_cfg)
+    extractor.modules["tokenizer"] = tokenizer
+    benchmark = DummyBenchmark({"fold": "s1", "rundocsonly": True})
+
+    extractor.create(
+        ["301"],
+        ["LA010189-0001", "LA010189-0002"],
+        benchmark.topics[benchmark.query_type],
+    )
+    reranker.build()
+
+    # 3 batches, each of seq len 4, and embedding dim 8
+    embedding = torch.ones(3, 4, 8)
+    # Set the 2nd and last token in first batch as pad
+    embedding[0, 1] = torch.zeros(8)
+    embedding[0, 3] = torch.zeros(8)
+
+    # set the first and third token in second batch as pad
+    embedding[1, 0] = torch.zeros(8)
+    embedding[1, 2] = torch.zeros(8)
+
+    mask = reranker.model.get_mask(embedding)
+
+    assert torch.equal(mask[0], torch.tensor([
+        [0, float('-inf'), 0, float('-inf')],
+        [float('-inf'), float('-inf'), float('-inf'), float('-inf')],
+        [0, float('-inf'), 0, float('-inf')],
+        [float('-inf'), float('-inf'), float('-inf'), float('-inf')]
+        ], dtype=torch.float))
+
+    assert torch.equal(mask[1], torch.tensor([
+        [float('-inf'), float('-inf'), float('-inf'), float('-inf')],
+        [float('-inf'), 0, float('-inf'), 0],
+        [float('-inf'), float('-inf'), float('-inf'), float('-inf')],
+        [float('-inf'), 0, float('-inf'), 0]
+
+    ], dtype=torch.float))
+
+    assert torch.equal(mask[2], torch.zeros(4, 4, dtype=torch.float))
