6254dee58552a340d834d62d7f9f720f79dbe381
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-04-02 15:04:56 +0200

    Add fake metric

--- capreolus/trainer/__init__.py
+++ capreolus/trainer/__init__.py
@@ -180,7 +180,7 @@ class PytorchTrainer(Trainer):
         hyperparams = dict(self.cfg)
         hyperparams.update(dict(reranker.cfg))
         hyperparams.update(dict(reranker["extractor"].cfg))
-        summary_writer.add_hparams(hyperparams)
+        summary_writer.add_hparams(hyperparams, {'hparams/fake': 0})
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = reranker.model.to(self.device)
