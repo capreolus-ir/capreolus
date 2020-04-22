aa7ff1bb3675944bfdc789a844b12b68d74b46e2
Crystina <xzhangbx@ust.hk>
2020-04-22 17:30:52 -0400

    first version of csn dataset; download_if_missing passed

--- capreolus/tests/test_collection.py
+++ capreolus/tests/test_collection.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from capreolus.index import AnseriniIndex
-from capreolus.collection import ANTIQUE
+from capreolus.collection import ANTIQUE, CodeSearchNet
 
 
 def test_antique_downloadifmissing():
@@ -16,3 +16,17 @@ def test_antique_downloadifmissing():
 
     index.create_index()
     assert index.exists()
+
+
+def test_csn_downloadifmissing():
+    cfg = {"_name": "codesearchnet", "lang": "ruby"}
+    col = CodeSearchNet(cfg)
+
+    # make sure index can be built on this collection
+    cfg = {"_name": "anserini", "indexstops": False, "stemmer": "porter"}
+    index = AnseriniIndex(cfg)
+    index.modules["collection"] = col
+
+    index.create_index()
+    assert index.exists()
+
