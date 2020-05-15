2e7571b92470d61af31cea42b0047b3963d9c010
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-05-15 14:13:19 +0200

    Fix tests, black formatter changes

--- capreolus/trainer/test_trainer.py
+++ capreolus/trainer/test_trainer.py
@@ -9,14 +9,16 @@ from trainer import TensorFlowTrainer
 
 def test_tf_get_tf_dataset(monkeypatch):
     benchmark = DummyBenchmark({"fold": "s1", "rundocsonly": True})
-    extractor = EmbedText({"keepstops": True})
+    extractor = EmbedText({"keepstops": True, "maxdoclen": 4, "maxqlen": 4})
     training_judgments = benchmark.qrels.copy()
     train_dataset = TrainDataset(training_judgments, training_judgments, extractor)
+
+    reranker = {"extractor": extractor}
 
     def mock_id2vec(*args, **kwargs):
         return {
             "query": np.array([1, 2, 3, 4], dtype=np.long),
-            "posdoc": np.array([1, 1, 1, 1], dtype=np.float),
+            "posdoc": np.array([1, 1, 1, 1], dtype=np.long),
             "negdoc": np.array([2, 2, 2, 2], dtype=np.long),
             "qid": "1",
             "posdocid": "posdoc1",
@@ -28,9 +30,7 @@ def test_tf_get_tf_dataset(monkeypatch):
     trainer = TensorFlowTrainer(
         {
             "_name": "tensorflow",
-            "maxdoclen": 4,
-            "maxqlen": 4,
-            "batch": 8,
+            "batch": 2,
             "niters": 2,
             "itersize": 16,
             "gradacc": 1,
@@ -46,18 +46,18 @@ def test_tf_get_tf_dataset(monkeypatch):
         }
     )
 
-    tf_record_filenames = trainer.convert_to_tf_train_record(train_dataset)
+    tf_record_filenames = trainer.convert_to_tf_train_record(reranker, train_dataset)
     for filename in tf_record_filenames:
         assert os.path.isfile(filename)
 
-    tf_record_dataset = trainer.load_tf_records_from_file(tf_record_filenames)
-    dataset = tf_record_dataset.batch(2)
+    tf_record_dataset = trainer.load_tf_records_from_file(reranker, tf_record_filenames, 2)
+    dataset = tf_record_dataset
 
     for idx, data_and_label in enumerate(dataset):
         batch, _ = data_and_label
-        tf.debugging.assert_equal(batch[0], tf.convert_to_tensor(np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), dtype=tf.float32))
-        tf.debugging.assert_equal(batch[1], tf.convert_to_tensor(np.array([[2, 2, 2, 2], [2, 2, 2, 2]]), dtype=tf.float32))
-        tf.debugging.assert_equal(batch[2], tf.convert_to_tensor(np.array([[1, 2, 3, 4], [1, 2, 3, 4]]), dtype=tf.float32))
+        tf.debugging.assert_equal(batch[0], tf.convert_to_tensor(np.array([[1, 1, 1, 1], [1, 1, 1, 1]]), dtype=tf.int64))
+        tf.debugging.assert_equal(batch[1], tf.convert_to_tensor(np.array([[2, 2, 2, 2], [2, 2, 2, 2]]), dtype=tf.int64))
+        tf.debugging.assert_equal(batch[2], tf.convert_to_tensor(np.array([[1, 2, 3, 4], [1, 2, 3, 4]]), dtype=tf.int64))
         tf.debugging.assert_equal(
             batch[3], tf.convert_to_tensor(np.array([[0.1, 0.1, 0.2, 0.1], [0.1, 0.1, 0.2, 0.1]]), dtype=tf.float32)
         )
