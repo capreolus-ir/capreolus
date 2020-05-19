a2cebd1d4769807e0443a0876a27c4b8018a5bde
Ghazaleh Haratinezhad Torbati <ghazale.hnt@gmail.com>
2020-05-19 17:24:58 +0200

    loading wikipedia2vec embedding from bin normed

--- capreolus/entityutils/__init__.py
+++ capreolus/entityutils/__init__.py
@@ -16,7 +16,6 @@ class EntityUtilsWiki2vec(EntityUtils):
     name = "wiki2vec"
 
     embedding_file = "/GW/PKB/nobackup/wikipedia2vec_pretrained/enwiki_20180420_300d"
-    @staticpro
     wiki2vec = None
 
     def load_pretrained_emb(self):
