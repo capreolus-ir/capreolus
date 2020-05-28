2d7cfb506f61ba8f454eaf5eae1349dae9052e8b
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-05-28 12:22:05 +0200

    Loop over the whole document

--- capreolus/reranker/TFBERTMaxP.py
+++ capreolus/reranker/TFBERTMaxP.py
@@ -38,7 +38,7 @@ class TFBERTMaxP_Class(tf.keras.Model):
         ones = tf.ones([batch_size, 1], dtype=tf.int64)
 
         passagelen = self.config["passagelen"]
-        # overlap = self.config["overlap"]
+        overlap = self.config["overlap"]
         pos_passage_scores = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
         neg_passage_scores = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
 
@@ -80,10 +80,10 @@ class TFBERTMaxP_Class(tf.keras.Model):
         # negdoc_score = tf.math.reduce_max(neg_passage_scores, axis=1)transpose
 
         def condition(idx, _pos_passage_scores, _neg_passage_scores):
-            return tf.less(idx, 2)
+            return tf.less(idx * (passagelen - overlap), doclen)
 
         def body(idx, _pos_passage_scores, _neg_passage_scores):
-            i = idx * passagelen
+            i = idx * (passagelen - overlap)
             pos_passage = pos_toks[:, i: i + passagelen]
             pos_passage_mask = posdoc_mask[:, i: i + passagelen]
             neg_passage = neg_toks[:, i:i + passagelen]
