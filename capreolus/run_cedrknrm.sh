153bfe1a370279b30c50d12dba3acab386c07937
Andrew Yates <andrew.yates@gmail.com>
2020-06-07 00:22:38 +0200

    old file

--- capreolus/run_cedrknrm.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 nohup python train.py with \
-  reranker=CEDRKNRM \
-  collection=rob04_cedr \
-  benchmark=robust04.title.cedr \
-  niters=2 itersize=512\
-  niters=50 vanillaiters=50 expid=cedrknrm_cmd \
-  earlystopping=False esepoch=15 freezebert=False batch=2 gradacc=8 &
