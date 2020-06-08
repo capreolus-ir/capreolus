2094bf1ac67c9573330bbf584b5a41503a7e23f1
Kevin Martin Jose <youcancallmekevin@gmail.com>
2020-06-08 02:36:04 +0200

    Revert back to from_pretrained

--- capreolus/reranker/TFBERTMaxP.py
+++ capreolus/reranker/TFBERTMaxP.py
@@ -14,9 +14,7 @@ class TFBERTMaxP_Class(tf.keras.Model):
         # self.clsidx = extractor.clsidx  # The index of the CLS token
         # self.sepidx = extractor.sepidx  # The index of the SEP token
         self.extractor = extractor
-        bert_config = BertConfig(config["pretrained"])
-        bert_config.hidden_dropout_prob = config["dropout"]
-        self.bert = TFBertForSequenceClassification.from_config(bert_config.to_dict())
+        self.bert = TFBertForSequenceClassification.from_pretrained(config["pretrained"])
         self.config = config
         self.aggregate_fn = self.get_aggregate_fn()
 
