1f178c4ae176bb92b17a43ee625ec4df06634f14
Andrew Yates <andrew.yates@gmail.com>
2020-07-01 10:51:15 +0200

    black

--- capreolus/reranker/PACRR.py
+++ capreolus/reranker/PACRR.py
@@ -4,6 +4,7 @@ from torch.nn import functional as F
 
 from capreolus import ConfigOption
 from capreolus.reranker import Reranker
+
 # TODO add shuffle, cascade, disambig?
 from capreolus.reranker.common import SimilarityMatrix, create_emb_layer
 
