0018f919b54acb8498de2930ae366790aa27111e
Crystina <xzhangbx@ust.hk>
2020-07-11 22:39:46 -0400

    adding test code for covid round3 qrel conversion

--- capreolus/tests/test_benchmark.py
+++ capreolus/tests/test_benchmark.py
@@ -1,13 +1,20 @@
 import os
 import pickle
+from collections import defaultdict
 
 import pytest
 from tqdm import tqdm
 
 from capreolus import Benchmark, module_registry
+from capreolus.utils.common import download_file
+from capreolus.utils.trec import load_qrels
+
 from capreolus.benchmark.codesearchnet import CodeSearchNetChallenge as CodeSearchNetCodeSearchNetChallengeBenchmark
 from capreolus.benchmark.codesearchnet import CodeSearchNetCorpus as CodeSearchNetCodeSearchNetCorpusBenchmark
 from capreolus.collection.codesearchnet import CodeSearchNet as CodeSearchNetCollection
+from capreolus.collection.covid import COVID as CovidCollection
+from capreolus.benchmark.covid import COVID as CovidBenchmark
+
 from capreolus.tests.common_fixtures import tmpdir_as_cache
 from capreolus.utils.common import remove_newline
 from capreolus.utils.loginit import get_logger
@@ -103,3 +110,33 @@ def test_csn_challenge_download_if_missing():
     benmchmark.download_if_missing()
 
     assert benmchmark.qid_map_file.exists() and benmchmark.topic_file.exists()
+
+
+# @pytest.mark.download
+def test_covid_round3_qrel_conversion():
+    collection_config = {"name": "covid", "round": 3, "coll_type": "abstract"}
+    benchmark_config = {"name": "covid", "udelqexpand": False, "useprevqrels": False}
+    collection = CovidCollection(collection_config)
+    benchmark = CovidBenchmark(benchmark_config, provide={"collectoin": collection})
+
+    benchmark.download_if_missing()
+
+    docid_map_tmp = "/tmp/docid.map"
+    newdocid_qrels_fn = "/tmp/new.docid.qrels"
+    qrel_url = "https://ir.nist.gov/covidSubmit/data/qrels-covid_d3_j0.5-3.txt"
+    docid_map_url = "https://ir.nist.gov/covidSubmit/data/changedIds-May19.csv"
+
+    download_file(docid_map_url, docid_map_tmp)
+    download_file(qrel_url, newdocid_qrels_fn)
+    with open(docid_map_tmp) as f:
+        old2new = {line.split(",")[0]: line.split(",")[1] for line in f}
+    newdocid_qrels = load_qrels(newdocid_qrels_fn)
+    olddocid_qrels = benchmark.qrels
+
+    # since there are dropped out terms in benchmark.qrels (the ones that appeared in previous judgements)
+    # converted olddocid_qrels will have less entries than newdocid_qrels.
+    # Cannot use assert convert_qrels == newdocid_qrels here
+    for qid in olddocid_qrels:
+        for docid in olddocid_qrels[qid]:
+            newdocid = old2new.get(docid, docid)
+            assert olddocid_qrels[qid][docid] == newdocid_qrels[qid][newdocid]
