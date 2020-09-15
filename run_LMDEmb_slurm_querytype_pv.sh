eac89e059432987202c31e71a404686f6fac2132
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-15 18:53:12 +0200

    not running on certain profile types

--- run_LMDEmb_slurm_querytype_pv.sh
+++ run_LMDEmb_slurm_querytype_pv.sh
@@ -8,20 +8,43 @@ entitystrategy=$4
 CPUNUM=4
 
 declare -a dstypes=("all_domains_tf_k-1" "all_domains_df_k-1" "amazon_tf_k-1" "amazon_df_k-1")
-declare -a pvtypes=("topic-alltopics_tf_k-1" "topic-amazon_tf_k-1" "user-allusers_tf_k-1")
+declare -a pvtypestopic=("topic-alltopics_tf_k-1" "topic-amazon_tf_k-1")
+declare -a pvtypesuser=("user-allusers_tf_k-1")
 
-for filterq in "${pvtypes[@]}"
+for filterq in "${pvtypestopic[@]}"
 do
-  sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${filterq}_%j.log run_LMDEmb_single_pv.sh $domain $pipeline $querytype $entitystrategy $filterq ;
-  sleep 30
+  if [ "$querytype" != "query" ] && [ "$querytype" != "basicprofile" ] && [ "$querytype" != "chatprofile" ]; then
+    sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${filterq}_%j.log run_LMDEmb_single_pv.sh $domain $pipeline $querytype $entitystrategy $filterq ;
+    sleep 30
+  fi
+done
+for filterq in "${pvtypestopic[@]}"
+do
+  if [ "$querytype" != "query" ]; then
+    sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${filterq}_%j.log run_LMDEmb_single_pv.sh $domain $pipeline $querytype $entitystrategy $filterq ;
+    sleep 30
+  fi
 done
 
-for filterq in "${pvtypes[@]}"
+for filterq in "${pvtypestopic[@]}"
 do
-  for domainvocsp in "${dstypes[@]}"
-  do
-    sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_LMDEmb_single_dv_pv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp $filterq ;
+  if [ "$querytype" != "query" ] && [ "$querytype" != "basicprofile" ] && [ "$querytype" != "chatprofile" ]; then
+    for domainvocsp in "${dstypes[@]}"
+    do
+      sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_LMDEmb_single_dv_pv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp $filterq ;
+      sleep 30
+    done
     sleep 30
-  done
-  sleep 30
+  fi
+done
+for filterq in "${pvtypestopic[@]}"
+do
+  if [ "$querytype" != "query" ]; then
+    for domainvocsp in "${dstypes[@]}"
+    do
+      sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_LMDEmb_single_dv_pv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp $filterq ;
+      sleep 30
+    done
+    sleep 30
+  fi
 done
