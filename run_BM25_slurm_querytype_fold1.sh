3f9e4597ebcc41d82bbbaf2319e6701fb3eb8370
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-16 20:54:10 +0200

    small changes

--- run_BM25_slurm_querytype_fold1.sh
+++ run_BM25_slurm_querytype_fold1.sh
@@ -7,11 +7,4 @@ querytype=$3
 entitystrategy=$4
 CPUNUM=2
 
-sbatch  -a 1-1 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_1_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log --wait run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
-
-declare -a dstypes=("all_domains_tf_k-1" "all_domains_df_k-1" "amazon_tf_k-1" "amazon_df_k-1")
-for domainvocsp in "${dstypes[@]}"
-do
-  sbatch -a 1-1 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_1_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_%j.log run_BM25_single_dv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp;
-  sleep 30
-done
+sbatch  -a 1-1 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_1_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
