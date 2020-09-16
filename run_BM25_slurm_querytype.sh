ed3f86588c28847ad7cf26173d10cfd333410bc5
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-16 19:22:13 +0200

    small changes

--- run_BM25_slurm_querytype.sh
+++ run_BM25_slurm_querytype.sh
@@ -7,13 +7,13 @@ querytype=$3
 entitystrategy=$4
 CPUNUM=2
 
-echo "sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;"
-sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+echo "sbatch -a 2-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;"
+sbatch -a 2-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
 
 declare -a dstypes=("all_domains_tf_k-1" "all_domains_df_k-1" "amazon_tf_k-1" "amazon_df_k-1")
 for domainvocsp in "${dstypes[@]}"
 do
   sleep 20
-  echo "sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_%j.log run_BM25_single_dv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp;"
-  sbatch -a 1-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_%j.log run_BM25_single_dv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp;
+  echo "sbatch -a 2-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_%j.log run_BM25_single_dv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp;"
+  sbatch -a 2-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_%j.log run_BM25_single_dv.sh $domain $pipeline $querytype $entitystrategy $domainvocsp;
 done
