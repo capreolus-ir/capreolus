2e8c052a02258ed4c4b69c71c390c5255a622295
Ghazaleh Haratinezhad Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-16 19:22:59 +0200

    changes

--- run_slurm_test.sh
+++ run_slurm_test.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-logfolder=/GW/NeuralIR/nobackup/ghazaleh/logs_15092020/
+logfolder=/GW/D5data-13/ghazaleh/ranking_outputs/logs_17092020/
 domain=food
 pipeline=ENTITY_CONCEPT_JOINT_LINKING
-querytype=chatprofile_hobbies
-entitystrategy=domainOnlyNE
+querytype=query
+entitystrategy=noneE
 CPUNUM=2
 
-sbatch -a 2-10 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+sbatch -a 5-5 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+
+domain=book
+sbatch -a 5-5 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+
+domain=movie
+sbatch -a 5-5 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+
+domain=travel_wikivoyage
+sbatch -a 5-5 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_%a_${domain}_${querytype}_${pipeline}_${entitystrategy}_${domainvocsp}_${filterq}_%j.log run_BM25_single.sh $domain $pipeline $querytype $entitystrategy ;
+
+
