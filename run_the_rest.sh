50fdd502c6bef3c465a1d76dc804f97d1c61b7db
Ghazaleh Haratinezhad Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-18 08:28:14 +0200

    running scripts

---
+++ run_the_rest.sh
@@ -0,0 +1,12 @@
+./run_BM25s.sh ;
+sleep 100;
+./run_LMDs.sh ;
+sleep 100;
+./run_BM25_PVs.sh 
+sleep 100;
+./run_LMD_PVs.sh 
+#sleep 100;
+#ghazaleh@slurm-submit:/GW/PKB/work/ghazaleh/capreolus$ vim run_LMDEmbs.sh 
+#sleep 100;
+#ghazaleh@slurm-submit:/GW/PKB/work/ghazaleh/capreolus$ vim run_LMDEmb_PVs.sh 
+
