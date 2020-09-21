4ec798a48fa935d201fa94ce4f8bcd8445e92e3e
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-21 18:16:30 +0200

    change log file names

--- run_KNRM_slurm_querytype_train.sh
+++ run_KNRM_slurm_querytype_train.sh
@@ -6,12 +6,12 @@ pipeline=$2
 entitystrategy=noneE
 
 assessed_set=random20
-echo "sbatch -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_%a_${domain}_${pipeline}_${assessed_set}.log --open-mode=append run_KNRM_train_single.sh $domain $pipeline $entitystrategy $assessed_set;"
-sbatch  -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_%a_${domain}_${pipeline}_${assessed_set}.log --open-mode=append  run_KNRM_train_single.sh  $domain $pipeline $entitystrategy $assessed_set;
+echo "sbatch -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_${domain}_${pipeline}_${assessed_set}.log --open-mode=append run_KNRM_train_single.sh $domain $pipeline $entitystrategy $assessed_set;"
+sbatch  -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_${domain}_${pipeline}_${assessed_set}.log --open-mode=append  run_KNRM_train_single.sh  $domain $pipeline $entitystrategy $assessed_set;
 
 ./waitgpu.sh;
 
 assessed_set=top10
-echo "sbatch -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_%a_${domain}_${pipeline}_${assessed_set}.log --open-mode=append run_KNRM_train_single.sh $domain $pipeline $entitystrategy $assessed_set;"
-sbatch  -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_%a_${domain}_${pipeline}_${assessed_set}.log --open-mode=append  run_KNRM_train_single.sh  $domain $pipeline $entitystrategy $assessed_set;
+echo "sbatch -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_${domain}_${pipeline}_${assessed_set}.log --open-mode=append run_KNRM_train_single.sh $domain $pipeline $entitystrategy $assessed_set;"
+sbatch  -a 1-1000 -p gpu20 --mem-per-cpu=64G -o ${logfolder}train_gpu_KNRM_${domain}_${pipeline}_${assessed_set}.log --open-mode=append  run_KNRM_train_single.sh  $domain $pipeline $entitystrategy $assessed_set;
 ./waitgpu.sh;
