9d6f803d8a0a9ee9acf7b1708b8de2573d37c793
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-22 16:20:21 +0200

    running scripts

--- run_LMD_slurm_querytype_pv.sh
+++ run_LMD_slurm_querytype_pv.sh
@@ -8,14 +8,14 @@ CPUNUM=2
 
 declare -a dstypes=("all_domains_tf_k-1" "all_domains_df_k-1" "amazon_tf_k-1" "amazon_df_k-1")
 
-echo "sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_${domain}_${pipeline}_${entitystrategy}_pv.log --open-mode=append run_LMD_single_pv.sh $domain $pipeline $entitystrategy ;"
-sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_${domain}_${pipeline}_${entitystrategy}_pv.log --open-mode=append run_LMD_single_pv.sh $domain $pipeline $entitystrategy ;
+echo "sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}LMD_${domain}_${pipeline}_${entitystrategy}_pv.log --open-mode=append run_LMD_single_pv.sh $domain $pipeline $entitystrategy ;"
+sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}LMD_${domain}_${pipeline}_${entitystrategy}_pv.log --open-mode=append run_LMD_single_pv.sh $domain $pipeline $entitystrategy ;
 
 ./wait.sh;
 
 for domainvocsp in "${dstypes[@]}"
 do
-  echo "sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_${domain}_${pipeline}_${entitystrategy}_${domainvocsp}_pv.log --open-mode=append run_LMD_single_dv_pv.sh $domain $pipeline $entitystrategy $domainvocsp ;"
-  sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}BM25_${domain}_${pipeline}_${entitystrategy}_${domainvocsp}_pv.log --open-mode=append run_LMD_single_dv_pv.sh $domain $pipeline $entitystrategy $domainvocsp ;
+  echo "sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}LMD_${domain}_${pipeline}_${entitystrategy}_${domainvocsp}_pv.log --open-mode=append run_LMD_single_dv_pv.sh $domain $pipeline $entitystrategy $domainvocsp ;"
+  sbatch -a 1-800 -p cpu20 -c $CPUNUM --mem-per-cpu=24G -o ${logfolder}LMD_${domain}_${pipeline}_${entitystrategy}_${domainvocsp}_pv.log --open-mode=append run_LMD_single_dv_pv.sh $domain $pipeline $entitystrategy $domainvocsp ;
   ./wait.sh;
 done
