7746949b7d4f870620d3c13055cbc51d15c42641
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-22 10:30:30 +0200

    bug

--- run_LMDEmb_slurm_querytype.sh
+++ run_LMDEmb_slurm_querytype.sh
@@ -3,8 +3,7 @@
 logfolder=/GW/D5data-13/ghazaleh/ranking_outputs/logs_18092020/
 domain=$1
 pipeline=$2
-querytype=$3
-entitystrategy=$4
+entitystrategy=$3
 CPUNUM=4
 
 echo "sbatch -a 1-200 -t 2-00:00:00 -p cpu20 -c $CPUNUM --mem-per-cpu=32G -o ${logfolder}LMDEmb_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMDEmb_single.sh $domain $pipeline $entitystrategy ;"
