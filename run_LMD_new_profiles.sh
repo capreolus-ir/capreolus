83f8d5f68686ef6e0289d3c4484958af6818961e
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-09-28 15:57:23 +0200

    run new profiles

--- run_LMD_new_profiles.sh
+++ run_LMD_new_profiles.sh
@@ -10,60 +10,60 @@ domainvocsp=None
 method=LMD
 
 entitystrategy=noneE
-sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 
 sleep 600;
 entitystrategy=allE
-sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 
 sleep 600;
 entitystrategy=domainE
-sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 
 sleep 600;
 entitystrategy=onlyNE
-sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 
 sleep 600;
 entitystrategy=domainOnlyNE
-sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1001-1010 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1051-1060 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1101-1110 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1151-1160 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
 sleep 10;
-sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_LMD_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+sbatch -p cpu20 -c 2 -a 1201-1210 --mem-per-cpu=64G -o ${logfolder}LMD_newprofiles_${domain}_${pipeline}_${entitystrategy}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
