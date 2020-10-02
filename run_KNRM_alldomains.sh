ffe3f55f8e0020a976e7058eb31ee0ef6ec4ad2e
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-10-02 09:52:05 +0200

    KNRM alldomains

---
+++ run_KNRM_alldomains.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+logfolder=/GW/D5data-13/ghazaleh/ranking_outputs/logs_30092020/
+pipeline=ENTITY_CONCEPT_JOINT_LINKING
+
+entitystrategy=noneE
+
+step=$1
+if [ "$step" == "" ];then
+  echo "give input step train or evaluate"
+  exit
+fi
+
+domain=alldomains
+declare -a doccuttypes=("None" "most_frequent")
+
+assessed_set=random20
+for doccut in "${doccuttypes[@]}"
+do
+    sbatch -p cpu20 -c 4 -a 1-1260 --mem-per-cpu=64G -o ${logfolder}${step}_KNRM_${domain}_${entitystrategy}_${pipeline}_${doccut}_${assessed_set}.log --open-mode=append run_KNRM_single_complete.sh  $domain $pipeline $entitystrategy $doccut $assessed_set $step;
+    sleep 60;
+done
+sleep 60;
+assessed_set=top10
+for doccut in "${doccuttypes[@]}"
+do
+  sbatch -p cpu20 -c 4 -a 1-1260 --mem-per-cpu=64G -o ${logfolder}${step}_KNRM_${domain}_${entitystrategy}_${pipeline}_${doccut}_${assessed_set}.log --open-mode=append run_KNRM_single_complete.sh  $domain $pipeline $entitystrategy $doccut $assessed_set $step;
+  sleep 60;
+done
+
