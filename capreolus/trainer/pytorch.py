capreolus-ir/capreolus

commit 454e77a08efdf1d1addff90d5bae3e87f533f2f6

Birch hacks

capreolus/trainer/pytorch.py
@@ -264,12 +264,7 @@ class PytorchTrainer(Trainer):
         # TODO should we write a /done so that training can be skipped if possible when fastforward=False? or in Task?
 
     def load_best_model(self, reranker, train_output_path):
-        self.optimizer = torch.optim.Adam(
-            filter(lambda param: param.requires_grad, reranker.model.parameters()), lr=self.config["lr"]
-        )
-
-        dev_best_weight_fn = train_output_path / "dev.best"
-        reranker.load_weights(dev_best_weight_fn, self.optimizer)
+        pass
 
     def predict(self, reranker, pred_data, pred_fn):
         """Predict query-document scores on `pred_data` using `model` and write a corresponding run file to `pred_fn`

capreolus/reranker/birch.py
@@ -26,8 +26,9 @@ class Birch_Class(nn.Module):
     def _load_bert(self):
         from transformers import BertTokenizer, BertForNextSentencePrediction
 
-        bert = BertForNextSentencePrediction.from_pretrained("bert-large-uncased")
-        bert.load_state_dict(torch.load("/GW/NeuralIR/nobackup/birch-emnlp_bert4ir_v2/models/converted"))
+        bert = BertForNextSentencePrediction.from_pretrained("bert-base-uncased")
+        saved_bert = torch.load("/GW/NeuralIR/nobackup/birch/models/saved.tmp_1")["model"]
+        bert.load_state_dict(saved_bert.state_dict())
         self.bert = bert
 
     def forward(self, k, doc, seg, mask):
@@ -102,9 +103,11 @@ class Birch_Class(nn.Module):
 
         real_out = torch.cat(out, dim=0)
         found_passages = real_out.shape[0]
-        pad_out = torch.min(real_out, dim=0)[0].repeat(needed_passages - found_passages, 1)
-
-        return torch.cat((real_out, pad_out), dim=0).cpu()
+        if found_passages < needed_passages:
+            pad_out = torch.min(real_out, dim=0)[0].repeat(needed_passages - found_passages, 1)
+            return torch.cat((real_out, pad_out), dim=0).cpu()
+        else:
+            return real_out
 
 
 @Reranker.register
@@ -131,9 +134,9 @@ class Birch(Reranker):
 
     def score(self, d):
         return [
-            self.model(d["poskey"], d["posdoc"], d["posdoc_seg"], d["posdoc_mask"]).view(-1),
-            self.model(d["negkey"], d["negdoc"], d["negdoc_seg"], d["negdoc_mask"]).view(-1),
+            self.model(d["poskey"], d["pos_bert_input"], d["pos_seg"], d["pos_mask"]).view(-1),
+            self.model(d["negkey"], d["neg_bert_input"], d["neg_seg"], d["neg_mask"]).view(-1),
         ]
 
     def test(self, d):
-        return self.model(d["poskey"], d["posdoc"], d["posdoc_seg"], d["posdoc_mask"]).view(-1)
+        return self.model(d["poskey"], d["pos_bert_input"], d["pos_seg"], d["pos_mask"]).view(-1)

capreolus/task/rerank.py
@@ -141,6 +141,7 @@ class RerankTask(Task):
             qids=best_search_run.keys(), docids=docids, topics=self.benchmark.topics[self.benchmark.query_type]
         )
         train_output_path = self.get_results_path()
+        self.reranker.build_model()
         self.reranker.trainer.load_best_model(self.reranker, train_output_path)
 
         test_run = defaultdict(dict)
@@ -150,9 +151,8 @@ class RerankTask(Task):
                 for idx, (docid, score) in enumerate(docs.items()):
                     if idx >= threshold:
                         break
-                test_run[qid][docid] = score
+                    test_run[qid][docid] = score
 
-        test_run = {qid: docs for qid, docs in best_search_run.items() if qid in self.benchmark.folds[fold]["predict"]["test"]}
         test_dataset = PredSampler()
         test_dataset.prepare(
             test_run, self.benchmark.qrels, self.reranker.extractor, relevance_level=self.benchmark.relevance_level
@@ -163,6 +163,16 @@ class RerankTask(Task):
         preds = {"test": test_preds}
 
         return preds
+
+    def bircheval(self):
+        fold = self.config["fold"]
+        train_output_path = self.get_results_path()
+        searcher_runs, reranker_runs = self.find_birch_crossvalidated_results()
+
+        fold_test_metrics = evaluator.eval_runs(
+            reranker_runs[fold]["test"], self.benchmark.qrels, evaluator.DEFAULT_METRICS, self.benchmark.relevance_level
+        )
+        logger.info("rerank: fold=%s test metrics: %s", fold, fold_test_metrics)
 
     def evaluate(self):
         fold = self.config["fold"]
@@ -246,5 +256,21 @@ class RerankTask(Task):
 
                 dev_path = Path(dev_output_path.as_posix().replace("fold-" + self.config["fold"], "fold-" + fold))
                 reranker_runs.setdefault(fold, {})["dev"] = Searcher.load_trec_run(dev_path)
+
+        return searcher_runs, reranker_runs
+    
+    def find_birch_crossvalidated_results(self):
+        searcher_runs = {}
+        rank_results = self.rank.evaluate()
+        reranker_runs = {}
+        train_output_path = self.get_results_path()
+        test_output_path = train_output_path / "pred" / "test" / "best"
+        dev_output_path = train_output_path / "pred" / "dev" / "best"
+        for fold in self.benchmark.folds:
+            # TODO fix by using multiple Tasks
+            test_path = Path(test_output_path.as_posix().replace("fold-" + self.config["fold"], "fold-" + fold))
+            if os.path.exists(test_path):
+                reranker_runs.setdefault(fold, {})["test"] = Searcher.load_trec_run(test_path)
+
 
         return searcher_runs, reranker_runs

commit c97ff5645075d4889792ff331b2510112525caff

Fix pytorch best_weights regression (#109)
Fixes dev weight selection in the PyTorch trainer. Broken in 454e77a.

capreolus/trainer/pytorch.py
@@ -253,6 +253,8 @@ class PytorchTrainer(Trainer):
                 summary_writer.add_scalar("P_20", metrics["P_20"], niter)
                 # write best dev weights to file
                 if metrics[metric] > dev_best_metric:
+                    dev_best_metric = metrics[metric]
+                    logger.info("new best dev metric: %0.4f", dev_best_metric)
                     reranker.save_weights(dev_best_weight_fn, self.optimizer)
 
             # write train_loss to file
@@ -268,7 +270,12 @@ class PytorchTrainer(Trainer):
         # TODO should we write a /done so that training can be skipped if possible when fastforward=False? or in Task?
 
     def load_best_model(self, reranker, train_output_path):
-        pass
+        self.optimizer = torch.optim.Adam(
+            filter(lambda param: param.requires_grad, reranker.model.parameters()), lr=self.config["lr"]
+        )
+
+        dev_best_weight_fn = train_output_path / "dev.best"
+        reranker.load_weights(dev_best_weight_fn, self.optimizer)
 
     def predict(self, reranker, pred_data, pred_fn):
         """Predict query-document scores on `pred_data` using `model` and write a corresponding run file to `pred_fn`

capreolus/trainer/tensorflow.py
@@ -207,8 +207,8 @@ class TensorflowTrainer(Trainer):
                     metrics = evaluator.eval_runs(trec_preds, dict(qrels), evaluator.DEFAULT_METRICS, relevance_level)
                     logger.info("dev metrics: %s", " ".join([f"{metric}={v:0.3f}" for metric, v in sorted(metrics.items())]))
                     if metrics[metric] > best_metric:
-                        logger.info("Writing checkpoint")
                         best_metric = metrics[metric]
+                        logger.info("new best dev metric: %0.4f", best_metric)
                         wrapped_model.save_weights("{0}/dev.best".format(train_output_path))
 
             if num_batches >= self.config["niters"] * self.config["itersize"]: