4e42b58f9381d7d4a1e22700a9b43afe6990c2f1
Ghazaleh H. Torbati <ghazaleh@mpi-inf.mpg.de>
2020-12-07 16:08:14 +0100

    run scripts prepared

--- run_METHOD.sh
+++ run_METHOD.sh
@@ -9,64 +9,31 @@ if [ "$method" == "" ];then
   exit
 fi
 
-declare -a domains=('book' 'food' 'movie' 'travel_wikivoyage')
+MEM=32
+SIMULRUN=10
+
+if [ "$method" == "LMDEmb" ];then
+  MEM=64
+fi
+
+declare -a domains=('book' 'food' 'travel')
 declare -a dvtypes=("None" "all_domains_tf_k-1" "all_domains_df_k-1" "amazon_tf_k-1" "amazon_df_k-1")
+declare -a ents=('noneE' 'allE' 'domainE' 'onlyNE' 'domainOnlyNE')
+if [ "$method" == "LMDEmb" ];then
+  declare -a ents=('noneE')
+fi
 
-entitystrategy=noneE
-for domainvocsp in "${dvtypes[@]}"
+for entitystrategy in "${ents[@]}"
 do
-  for domain in "${domains[@]}"
+  for domainvocsp in "${dvtypes[@]}"
   do
-    echo "sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
-    sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
-    sleep 10;
+    for domain in "${domains[@]}"
+    do
+      echo "sbatch -p cpu20 -c 2 -a 1-850%${SIMULRUN} --mem-per-cpu=32G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
+      sbatch -p cpu20 -c 2 -a 1-850%${SIMULRUN} --mem-per-cpu=32G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
+      sleep 5;
+    done
+    sleep 60;
   done
+  sleep 3600; #wait an hour!
 done
-#sleep 300;
-
-#entitystrategy=allE
-#for domainvocsp in "${dvtypes[@]}"
-#do
-#  for domain in "${domains[@]}"
-#  do
-#    echo "sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
-#    sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
-#    sleep 60;
-#  done
-#done
-#sleep 300;
-
-#entitystrategy=domainE
-#for domainvocsp in "${dvtypes[@]}"
-#do
-#  for domain in "${domains[@]}"
-#  do
-#    echo "sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
-#    sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
-#    sleep 60;
-#  done
-#done
-#sleep 300;
-
-#entitystrategy=onlyNE
-#for domainvocsp in "${dvtypes[@]}"
-#do
-#  for domain in "${domains[@]}"
-#  do
-#    echo "sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
-#    sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
-#    sleep 60;
-#  done
-#done
-#sleep 300;
-
-#entitystrategy=domainOnlyNE
-#for domainvocsp in "${dvtypes[@]}"
-#do
-#  for domain in "${domains[@]}"
-#  do
-#    echo "sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;"
-#    sbatch -p cpu20 -c 2 -a 1-1050 --mem-per-cpu=64G -o ${logfolder}${method}_${domain}_${entitystrategy}_${pipeline}_${domainvocsp}.log --open-mode=append run_STATISTICALs_single_complete.sh  $domain $pipeline $entitystrategy $domainvocsp $method;
-#    sleep 60;
-#  done
-#done
