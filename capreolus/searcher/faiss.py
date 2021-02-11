ed5544bccd4da675bb757448cfa0e2a7c27cdcb1
Kevin Martin Jose <youcancallmekevin@gmail.com>
2021-02-11 11:12:42 +0100

    Support for query expansion with FAISS

--- capreolus/searcher/faiss.py
+++ capreolus/searcher/faiss.py
@@ -1,9 +1,12 @@
 import torch
+import random
+import re
 import pickle
 from tqdm import tqdm
+import subprocess
 import os
 import numpy as np
-from capreolus import ConfigOption, Dependency, constants, evaluator
+from capreolus import ConfigOption, Dependency, constants, evaluator, Anserini
 from capreolus.utils.trec import load_trec_topics
 from capreolus import get_logger
 
@@ -32,10 +35,11 @@ class FAISSSearcher(Searcher):
     def _query_from_file(self, topicsfn, output_path, fold=None):
         assert fold is not None
 
+        self.build_encoder(fold)
+        topics = load_trec_topics(topicsfn)
         # `qid_query` contains (qid, query) tuples in the order they were encoded
-
+        topic_vectors, qid_query = self.create_topic_vectors(topics, fold)
         # A manual search is done over the docs in dev_run - this way for each qid, we only score the docids that BM25 retrieved for it
-        topic_vectors, qid_query = self.create_topic_vectors(topicsfn, fold)
         # self.index.manual_search_train_set(topic_vectors, qid_query, fold)
         self.index.manual_search_dev_set(topic_vectors, qid_query, fold)
         self.index.manual_search_test_set(topic_vectors, qid_query, fold)
@@ -44,8 +48,20 @@ class FAISSSearcher(Searcher):
         self.calc_faiss_search_metrics_for_dev_set(distances, results, qid_query, fold)
         self.calc_faiss_search_metrics_for_test_set(distances, results, qid_query, fold)
         distances = distances.astype(np.float16)
+        self.write_results_in_trec_format(results, distances, qid_query, output_path, filename="faiss.run")
 
-        self.write_results_in_trec_format(results, distances, qid_query, output_path)
+        expanded_topics = self.expand_queries(os.path.join(output_path, "faiss.run"))
+        expanded_topic_vectors, qid_query = self.create_topic_vectors(expanded_topics)
+        self.index.manual_search_dev_set(expanded_topic_vectors, qid_query, fold)
+        self.index.manual_search_test_set(expanded_topic_vectors, qid_query, fold)
+        distances, results = self.index.faiss_search(expanded_topic_vectors, 100, qid_query, fold)
+        self.calc_faiss_search_metrics_for_dev_set(distances, results, qid_query, fold)
+        self.calc_faiss_search_metrics_for_test_set(distances, results, qid_query, fold)
+        distances = distances.astype(np.float16)
+        self.write_results_in_trec_format(results, distances, qid_query, output_path, filename="faiss_expanded.run")
+
+        # Deleting the results obtained using the unexpanded queries
+        os.remove(os.path.join(output_path, "faiss.run"))
 
         return output_path
 
@@ -65,12 +81,11 @@ class FAISSSearcher(Searcher):
 
         self.index.encoder.build_model(train_run, dev_run, docids, encoder_qids)
 
-    def create_topic_vectors(self, topicsfn, fold):
+    def create_topic_vectors(self, topics, fold):
         """
         Creates a tensor of shape (num_queries, emb_size). Uses all the topics available in the dataset. Filtering based on folds is done later
         """
-        self.build_encoder(fold)
-        topics = load_trec_topics(topicsfn)
+
         # TODO: Use the test qids in the below line
 
         qid_query = sorted([(qid, query) for qid, query in topics["title"].items() if qid in self.benchmark.folds[fold]["predict"]["dev"] or qid in self.benchmark.folds[fold]["predict"]["test"]])
@@ -91,8 +106,64 @@ class FAISSSearcher(Searcher):
                 topic_vectors.append(topic_vector)
                 
         return np.concatenate(topic_vectors, axis=0), qid_query
-        
-    def write_results_in_trec_format(self, results, distances, qid_query, output_path):
+
+    def expand_queries(self, faiss_run_file):
+        index_path = self.index.index.get_index_path()
+        topicsfn = self.benchmark.topic_file
+        output_path = os.path.join(self.get_cache_path(), "expanded_queries.txt")
+        topicfield = "title"
+
+        cmd = [
+            "java",
+            "-classpath",
+            "/home/kjose/anserini/target/anserini-0.10.2-SNAPSHOT-fatjar.jar",
+            "-Xms512M",
+            "-Xmx31G",
+            "-Dapp.name=ExpandQueries",
+            "io.anserini.search.ExpandQueries",
+            "-topicreader",
+            "Trec",
+            "-runFile",
+            faiss_run_file,
+            "-index",
+            index_path,
+            "-topics",
+            topicsfn,
+            "-output",
+            output_path,
+            "-topicfield",
+            topicfield,
+            "-stemmer",
+            "none",
+        ]
+        app = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        for line in app.stdout:
+            Anserini.filter_and_log_anserini_output(line, logger)
+
+        app.wait()
+        if app.returncode != 0:
+            raise RuntimeError("command failed")
+
+        logger.info("Expanded queries written to: {}".format(output_path))
+
+        expanded_topics = self.load_expanded_topics(output_path)
+
+        return expanded_topics
+
+    def load_expanded_topics(self, expanded_topics_fn):
+        expanded_topics = {"title": {}}
+        with open(expanded_topics_fn, "r") as f:
+            for line in f:
+                qid, boosted_query = line.split("\t")
+                query_terms = re.findall('\(.*?\)', boosted_query)
+                query_terms = [s[1:-1] for s in query_terms]
+                expanded_topics["title"][qid] = " ".join(query_terms)
+                if random.random() > 0.9:
+                    logger.debug("Expanded query {} is: {}".format(qid, " ".join(query_terms)))
+
+        return expanded_topics
+
+    def write_results_in_trec_format(self, results, distances, qid_query, output_path, filename="faiss.run"):
         faiss_id_to_doc_id_fn = os.path.join(self.index.get_cache_path(), "faiss_id_to_doc_id.dump")
         faiss_id_to_doc_id = pickle.load(open(faiss_id_to_doc_id_fn, "rb"))
         trec_string = "{qid} 0 {doc_id} {rank} {score} faiss\n"
@@ -100,7 +171,7 @@ class FAISSSearcher(Searcher):
         assert num_queries == len(qid_query)
 
         os.makedirs(output_path, exist_ok=True)
-        with open(os.path.join(output_path, "faiss.run"), "w") as f:
+        with open(os.path.join(output_path, filename), "w") as f:
             for i in range(num_queries):
                 faiss_ids = results[i][results[i] > -1]
                 qid = qid_query[i][0]
