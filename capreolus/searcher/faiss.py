419b4567fe01a36998dbec47a48f71201e449725
Kevin Martin Jose <youcancallmekevin@gmail.com>
2021-02-11 13:33:02 +0100

    Make FAISS searcher cache path

--- capreolus/searcher/faiss.py
+++ capreolus/searcher/faiss.py
@@ -110,6 +110,7 @@ class FAISSSearcher(Searcher):
     def expand_queries(self, faiss_run_file):
         index_path = self.index.index.get_index_path()
         topicsfn = self.benchmark.topic_file
+        os.makedirs(self.get_cache_path(), exist_ok=True)
         output_path = os.path.join(self.get_cache_path(), "expanded_queries.txt")
         topicfield = "title"
 
