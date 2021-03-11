12ece9315ffca709ec673e9e3238cdb0713a751d
Andrew Yates <andrew.yates@gmail.com>
2021-03-11 16:40:46 +0100

    covid from irds with abstracts only

---
+++ capreolus/collection/covidabstract.py
@@ -0,0 +1,21 @@
+import json
+
+from capreolus import constants
+from capreolus.utils.loginit import get_logger
+
+from . import Collection, IRDCollection
+
+logger = get_logger(__name__)
+PACKAGE_PATH = constants["PACKAGE_PATH"]
+
+
+@Collection.register
+class CovidAbstract(IRDCollection):
+    """ TREC-COVID with only abstracts (no title or body) """
+
+    module_name = "covidabstract"
+    ird_dataset_name = "cord19/trec-covid"
+    collection_type = "JsonCollection"
+
+    def doc_as_json(self, doc):
+        return json.dumps({"id": doc.doc_id, "contents": doc.abstract})
