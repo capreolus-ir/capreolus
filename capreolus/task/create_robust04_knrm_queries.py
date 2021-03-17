bb4381860c5e219a025f3bd44e1d47103d34cc29
Kevin Martin Jose <youcancallmekevin@gmail.com>
2021-03-17 12:36:38 +0100

    Pass topics

--- capreolus/task/create_robust04_knrm_queries.py
+++ capreolus/task/create_robust04_knrm_queries.py
@@ -72,7 +72,7 @@ class Robust04SimmatQueries(Task):
 
         doc_to_generated_queries = defaultdict(lambda: 0)
         passage_to_generated_queries = defaultdict(list)
-        self.reranker.extractor.preprocess(list(qrels.keys()), list(relevant_docids_in_qrels))
+        self.reranker.extractor.preprocess(list(qrels.keys()), list(relevant_docids_in_qrels), self.benchmark.topics["title"])
 
         for doc_id, qid_list in tqdm(relevant_docid_to_query.items(), desc="Generate queries"):
             for qid in qid_list:
