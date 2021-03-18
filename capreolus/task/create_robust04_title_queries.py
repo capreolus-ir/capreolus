0ee4856f498dc100388f9826f3a0da0a4bddb36f
Kevin Martin Jose <youcancallmekevin@gmail.com>
2021-03-18 13:26:18 +0100

    Remove multi gpu support

--- capreolus/task/create_robust04_title_queries.py
+++ capreolus/task/create_robust04_title_queries.py
@@ -58,8 +58,8 @@ class Robust04Queries(Task):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         tokenizer = T5Tokenizer.from_pretrained('t5-base')
         config = T5Config.from_pretrained('t5-base')
-        t5_model = torch.nn.DataParallel(T5ForConditionalGeneration.from_pretrained(
-            '/GW/NeuralIR/nobackup/kevin_cache/msmarco_saved/docT5/model.ckpt-1004000', from_tf=True, config=config))
+        t5_model = T5ForConditionalGeneration.from_pretrained(
+            '/GW/NeuralIR/nobackup/kevin_cache/msmarco_saved/docT5/model.ckpt-1004000', from_tf=True, config=config)
         t5_model.to(device)
 
         qrels = self.benchmark.qrels
@@ -90,7 +90,7 @@ class Robust04Queries(Task):
 
                     passage = self.index.get_doc(passage_id)
                     input_ids = tokenizer.encode(passage + "</s>", return_tensors='pt').to(device)
-                    output = t5_model.module.generate(input_ids=input_ids, max_length=self.config["querylen"], do_sample=True, top_k=10,
+                    output = t5_model.generate(input_ids=input_ids, max_length=self.config["querylen"], do_sample=True, top_k=10,
                                                num_return_sequences=self.config["numqueries"])
                     generated_queries = [tokenizer.decode(output[i], skip_special_tokens=True) for i in
                                          range(self.config["numqueries"])]
