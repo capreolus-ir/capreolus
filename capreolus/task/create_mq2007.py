b6d7aafe8e3c1ce5c0f288f215c2124e3ccbaa07
Kevin Martin Jose <youcancallmekevin@gmail.com>
2021-03-23 18:03:24 +0100

    MQ2007 generation script

--- capreolus/task/create_mq2007.py
+++ capreolus/task/create_mq2007.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import json
 from capreolus import Dependency, ConfigOption
 from capreolus.task import Task
 from capreolus.utils.loginit import get_logger
@@ -20,7 +21,9 @@ class Robust04DescQueries(Task):
         ConfigOption("queryoutput", "/home/kjose/capreolus/capreolus/data/topics.mq2007.txt"),
         ConfigOption("qrelsoutput", "/home/kjose/capreolus/capreolus/data/qrels.mq2007.txt"),
         ConfigOption("foldsoutput", "/home/kjose/capreolus/capreolus/data/mq2007.folds.json"),
-
+        ConfigOption("gov2queryoutput", "/home/kjose/capreolus/capreolus/data/topics.gov2.txt"),
+        ConfigOption("gov2qrelsoutput", "/home/kjose/capreolus/capreolus/data/qrels.gov2.txt"),
+        ConfigOption("gov2foldsoutput", "/home/kjose/capreolus/capreolus/data/gov2.folds.json"),
     ]
 
     dependencies = [
@@ -28,7 +31,6 @@ class Robust04DescQueries(Task):
 
     commands = ["generate"] + Task.help_commands
     default_command = "generate"
-
 
     def generate(self):
         mq2007_dataset = ir_datasets.load("gov2/trec-mq-2007")
@@ -39,13 +41,13 @@ class Robust04DescQueries(Task):
         old_queries = set()
 
         for query in tb04_dataset.queries_iter():
-            old_queries.add(query.title)
+            old_queries.add(query.title.lower())
 
         for query in tb05_dataset.queries_iter():
-            old_queries.add(query.title)
+            old_queries.add(query.title.lower())
 
         for query in tb06_dataset.queries_iter():
-            old_queries.add(query.title)
+            old_queries.add(query.title.lower())
 
         assert len(old_queries) == 150
 
@@ -69,16 +71,92 @@ class Robust04DescQueries(Task):
             elif qid not in old_qrels:
                 new_qrels += 1
 
-        query_duplicates = 0
-        new_queries = 0
+        duplicate_queries = []
+        new_queries = []
         for query in mq2007_dataset.queries_iter():
             if query.text in old_queries:
-                query_duplicates += 1
+                duplicate_queries.append(query)
             else:
-                new_queries += 1
+                new_queries.append(query)
 
-        logger.info("There are {} duplicates, {} deeper judgements, and {} new judgements".format(duplicate_qrels, deeper_qrels, new_qrels))
-        logger.info("There are {} query duplicates and {} new queries".format(query_duplicates, new_queries))
+        assert len(duplicate_queries) == 150
+        logger.info("There are {} duplicate judgements, {} deeper judgements, and {} new judgements".format(duplicate_qrels, deeper_qrels, new_qrels))
+        logger.info("There are {} query duplicates and {} new queries".format(len(duplicate_queries), len(new_queries)))
+
+        new_query_ids = [query.query_id for query in new_queries]
+        old_set_1 = [query.query_id for query in duplicate_queries[:50]]
+        old_set_2 = [query.query_id for query in duplicate_queries[50:100]]
+        old_set_3 = [query.query_id for query in duplicate_queries[100: 150]]
+
+        folds = {
+            "s1": {
+                "train_qids": new_query_ids + old_set_1,
+                "predict": {
+                    "dev": old_set_2,
+                    "test": old_set_3
+                }
+            },
+            "s2": {
+                "train_qids": new_query_ids + old_set_2,
+                "predict": {
+                    "dev": old_set_3,
+                    "test": old_set_1
+                }
+            },
+            "s3": {
+                "train_qids": new_query_ids + old_set_3,
+                "predict": {
+                    "dev": old_set_1,
+                    "test": old_set_2
+                }
+            }
+        }
+        with open(self.config["foldsoutput"], "w") as out_f:
+            json.dump(folds, out_f)
+
+        with open(self.config["queryoutput"], "w") as out_f:
+            for query in new_queries:
+                out_f.write(topic_to_trectxt(query.query_id, query.text.lower()))
+
+        with open(self.config["qrelsoutput"], "w") as out_f:
+            for qrel in mq2007_dataset.qrels_iter():
+                out_f.write("{} 0 {} {}\n".format(qrel.query_id, qrel.doc_id, qrel.relevance))
+
+        gov2_folds = {
+            "s1": {
+                "train_qids": old_set_1,
+                "predict": {
+                    "dev": old_set_2,
+                    "test": old_set_3
+                }
+            },
+            "s2": {
+                "train_qids": old_set_2,
+                "predict": {
+                    "dev": old_set_3,
+                    "test": old_set_1
+                }
+            },
+            "s3": {
+                "train_qids": old_set_3,
+                "predict": {
+                    "dev": old_set_1,
+                    "test": old_set_2
+                }
+            }
+        }
+
+        with open(self.config["gov2foldsoutput", "w"]) as out_f:
+            json.dump(gov2_folds, out_f)
+
+        with open(self.config["gov2queryoutput"], "w") as out_f:
+            for query in duplicate_queries:
+                out_f.write(topic_to_trectxt(query.query_id, query.text.lower()))
+
+        with open(self.config["gov2qrelsoutput", "w"]) as out_f:
+            for qid, docid_to_label in old_qrels.items():
+                for docid, label in docid_to_label.items():
+                    out_f.write("{} 0 {} {}\n".format(qid, docid, label))
 
     def update_qrels(self, qrels_dict, dataset):
         for qrel in dataset.qrels_iter():
