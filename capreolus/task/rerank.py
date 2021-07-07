1a3a329ec292b49cba93cf6c3c2b81cc704f55a6
Crystina <x978zhan@uwaterloo.ca>
2021-07-07 16:58:25 +0200

    clean up log

--- capreolus/task/rerank.py
+++ capreolus/task/rerank.py
@@ -42,18 +42,10 @@ class RerankTask(Task):
     def train(self):
         fold = self.config["fold"]
 
-        t1 = time()
         self.rank.search()
-        logger.info(f"Time to rank.search: {time() - t1}")
-        t1 = time()
-
         rank_results = self.rank.evaluate()
-        logger.info(f"Time to rank.evaluate: {time() - t1}")
-        t1 = time()
         best_search_run_path = rank_results["path"][fold]
         best_search_run = Searcher.load_trec_run(best_search_run_path)
-        logger.info(f"Time to load best search run: {time() - t1}")
-
         return self.rerank_run(best_search_run, self.get_results_path())
 
     def rerank_run(self, best_search_run, train_output_path, include_train=False):
