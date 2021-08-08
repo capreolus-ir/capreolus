7664df9662df0411bd10470ba0649a2a268bf753
Crystina <x978zhan@uwaterloo.ca>
2021-08-08 15:21:21 -0400

    Update sample slurm script

--- docs/setup/scripts/sample_slurm_script.sh
+++ docs/setup/scripts/sample_slurm_script.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=msmarcopsg
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100:4
+#SBATCH --gres=gpu:v100l:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=120GB
 #SBATCH --time=48:00:00
 #SBATCH --account=your_slurm_account
 #SBATCH --cpus-per-task=10
-#SBATCH -o ./your_output_path
+#SBATCH -o ./output.log
 
-batch=16
-itersize=30000
-benchmark=msmarcopsg
-searcher=msmarcopsgbm25
-pretrained=bert-base-uncased # or /path/to/bert-base-uncased if the model folder is not moved to  `$HOME/setup_capr/src/capreolus`
+lr=1e-3
+bertlr=2e-5
+batch_size=16
+niters=10
+warmupiters=1
+decayiters=$niters  # either same with $itersize or 0
 
-source ~/.bashrc
-conda activate caprolus
-source /path/to/setup/dir/setup_capreolus_on_cc.bash  # e.g. $HOME/setup_capr/setup_capreolus_on_cc.bash
-
-capreolus rerank.train with \
-        benchmark.name=$benchmark \
-        rank.searcher.name=$searcher \
-        benchmark.collection.name=msmarcopsg \
-        reranker.extractor.numpassages=1  \
-        reranker.extractor.maxseqlen=512 \
-        reranker.extractor.maxqlen=50 \
-        reranker.name=TFBERTMaxP \
-        reranker.pretrained=$pretrained \
-        reranker.extractor.tokenizer.pretrained=$pretrained \
-        reranker.extractor.usecache=True \
-        reranker.trainer.usecache=True \
-        reranker.trainer.niters=1 \
-        reranker.trainer.itersize=$itersize \
-        reranker.trainer.batch=$batch \
-        optimize=MRR@10 rank.optimize=MRR@10
+python -m capreolus.run rerank.train with \
+    file=docs/reproduction/config_msmarco.txt  \
+    reranker.trainer.batch=$batch_size \
+    reranker.trainer.lr=$lr \
+    reranker.trainer.bertlr=$bertlr \
+    reranker.trainer.niters=$niters \
+    reranker.trainer.warmupiters=$warmupiters \
+    reranker.trainer.decayiters=$decayiters \
+    fold=s1
