c66fd788a9cfe2e7b71b92c342409cca4cc93d30
Crystina <x978zhan@uwaterloo.ca>
2022-01-09 20:21:36 +0100

    replace lce-sampler; rename it into LCE

--- capreolus/sampler/__init__.py
+++ capreolus/sampler/__init__.py
@@ -165,6 +165,36 @@ class TrainPairSampler(Sampler, TrainingSamplerMixin, torch.utils.data.IterableD
 
 
 @Sampler.register
+class LCETrainSampler(TrainTripletSampler):
+    module_name = "LCE"
+
+    config_spec = [
+        ConfigOption("nneg", 7, "Number of negative samples to include"),
+    ]
+
+    def generate_samples(self):
+        """Generates (pos, neg * n) infinitely."""
+        all_qids = sorted(self.qid_to_reldocs)
+        if len(all_qids) == 0:
+            raise RuntimeError("TrainDataset has no valid qids")
+
+        while True:
+            self.rng.shuffle(all_qids)
+
+            for qid in all_qids:
+                posdocid = self.rng.choice(self.qid_to_reldocs[qid])
+                negdocids = self.rng.choice(self.qid_to_negdocs[qid], self.config["nneg"])
+                label = [1] + [0] * self.config["nneg"]
+
+                try:
+                    yield self.extractor.id2vec(qid, posdocid, negdocids, label)
+                except MissingDocError:
+                    # at training time we warn but ignore on missing docs
+                    logger.warning(
+                        "skipping training pair with missing features: qid=%s posid=%s negid=%s", qid, posdocid, negdocids
+                    )
+
+@Sampler.register
 class PredSampler(Sampler, torch.utils.data.IterableDataset):
     """
     Creates a Dataset for evaluation (test) data to be used with a pytorch DataLoader
@@ -223,37 +253,6 @@ class PredSampler(Sampler, torch.utils.data.IterableDataset):
         for qid in self.qid_to_docids:
             for docid in self.qid_to_docids[qid]:
                 yield qid, docid
-
-
-@Sampler.register
-class LCETrainTripletSampler(TrainTripletSampler):
-    module_name = "LCEtriplet"
-
-    config_spec = [
-        ConfigOption("nneg", 7, "Number of negative samples to include"),
-    ]
-
-    def generate_samples(self):
-        """Generates (pos, neg * n) infinitely."""
-        all_qids = sorted(self.qid_to_reldocs)
-        if len(all_qids) == 0:
-            raise RuntimeError("TrainDataset has no valid qids")
-
-        while True:
-            self.rng.shuffle(all_qids)
-
-            for qid in all_qids:
-                posdocid = self.rng.choice(self.qid_to_reldocs[qid])
-                negdocids = self.rng.choice(self.qid_to_negdocs[qid], self.config["nneg"])
-                label = [1] + [0] * self.config["nneg"]
-
-                try:
-                    yield self.extractor.id2vec(qid, posdocid, negdocids, label)
-                except MissingDocError:
-                    # at training time we warn but ignore on missing docs
-                    logger.warning(
-                        "skipping training pair with missing features: qid=%s posid=%s negid=%s", qid, posdocid, negdocids
-                    )
 
 
 from profane import import_all_modules
