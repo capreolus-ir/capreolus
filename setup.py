fc9247d9acc672f55be03a2e860db10c4964ba86
Crystina <x978zhan@uwaterloo.ca>
2022-01-10 02:17:06 +0100

    tf-ranking == 0.3.3

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setuptools.setup(
         "oauth2client",
         "tensorflow>=2.3,<2.5",
         "transformers==4.6.0",
-        "tensorflow-ranking<0.5",
+        "tensorflow-ranking==0.3.3",
         "Pillow",
         "beautifulsoup4",
         "lxml",
